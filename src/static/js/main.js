e88364a19e369df7a706bef46f8ac05b84e2e89f
erim0m0 <amirj070@gmail.com>
2022-08-18 23:35:45 +0430

    update main.html

---
+++ src/static/js/main.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+	    //options here
+	        autoScrolling:true,
+	        scrollHorizontally: true,
+            navigation: true,
+            scrollingSpeed: 1500,
+            navigationTooltips: ['Home', 'About Us', 'Contact Us'],
+            showActiveTooltip: true,
+        });
