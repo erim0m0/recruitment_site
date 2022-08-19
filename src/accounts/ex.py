1bdf4420ff91d26931bd81de32cfca535d537e9a
erim0m0 <amirj070@gmail.com>
2022-08-19 20:19:15 +0430

    update main.html

--- src/accounts/ex.py
+++ src/accounts/ex.py
@@ -1,3 +1,8 @@
 import random
+import sys
+print(random.randint(111111, 999999))
 
-print(random.randint(111111,999999))
+b = ['a', 'b']
+print(sys.getsizeof(b))
+if (a := len(b)) < 10:
+    print(f'len of b is {a}')
